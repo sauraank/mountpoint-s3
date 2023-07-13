1ccc07c68b02a3d59adab5a4568e36217df27993
Ankit Saurabh <sauraank@amazon.co.uk>
2023-07-13 17:19:14 +0100

    Corrected errors in endpoint test, still need to correct a few in inode.rs
    Signed-off-by: Ankit Saurabh <sauraank@amazon.co.uk>

--- mountpoint-s3-client/tests/endpoint.rs
+++ mountpoint-s3-client/tests/endpoint.rs
@@ -5,10 +5,10 @@ pub mod common;
 use aws_sdk_s3::types::ByteStream;
 use bytes::Bytes;
 use common::*;
-use mountpoint_s3_client::{AddressingStyle, Endpoint, ObjectClient, S3ClientConfig, S3CrtClient, EndpointConfig};
+use mountpoint_s3_client::{AddressingStyle, Endpoint, EndpointConfig, ObjectClient, S3ClientConfig, S3CrtClient};
 use test_case::test_case;
 
-async fn run_test<F: FnOnce(&str) -> Endpoint>(f: F) {
+async fn run_test<F: FnOnce(&str) -> EndpointConfig>(f: F) {
     let sdk_client = get_test_sdk_client().await;
     let (bucket, prefix) = get_test_bucket_and_prefix("test_region");
 
@@ -25,8 +25,7 @@ async fn run_test<F: FnOnce(&str) -> Endpoint>(f: F) {
         .unwrap();
 
     let region = get_test_region();
-    let endpoint = f(&region);
-    let endpoint_config = EndpointConfig::new().region(&region).endpoint(endpoint);
+    let endpoint_config = f(&region);
     let config = S3ClientConfig::new().endpoint_config(endpoint_config.clone());
     let client = S3CrtClient::new(config).expect("could not create test client");
 
@@ -42,10 +41,7 @@ async fn run_test<F: FnOnce(&str) -> Endpoint>(f: F) {
 #[test_case(AddressingStyle::Path)]
 #[tokio::test]
 async fn test_addressing_style_region(addressing_style: AddressingStyle) {
-    run_test(|region| {
-        let endpoint_config = 
-        Endpoint::set_endpoint(region, addressing_style).unwrap()
-    }).await;
+    run_test(|region| EndpointConfig::new().region(region).addressing_style(addressing_style)).await;
 }
 
 #[test_case(AddressingStyle::Automatic)]
@@ -55,7 +51,11 @@ async fn test_addressing_style_region(addressing_style: AddressingStyle) {
 async fn test_addressing_style_uri(addressing_style: AddressingStyle) {
     run_test(|region| {
         let uri = format!("https://s3.{region}.amazonaws.com");
-        Endpoint::from_uri(&uri, addressing_style).unwrap()
+        let endpoint = Endpoint::from_uri(&uri).unwrap();
+        EndpointConfig::new()
+            .region(region)
+            .addressing_style(addressing_style)
+            .endpoint(endpoint)
     })
     .await;
 }
@@ -67,7 +67,11 @@ async fn test_addressing_style_uri(addressing_style: AddressingStyle) {
 async fn test_addressing_style_uri_dualstack(addressing_style: AddressingStyle) {
     run_test(|region| {
         let uri = format!("https://s3.dualstack.{region}.amazonaws.com");
-        Endpoint::from_uri(&uri, addressing_style).unwrap()
+        let endpoint = Endpoint::from_uri(&uri).unwrap();
+        EndpointConfig::new()
+            .region(region)
+            .addressing_style(addressing_style)
+            .endpoint(endpoint)
     })
     .await;
 }
@@ -78,7 +82,11 @@ async fn test_addressing_style_uri_dualstack(addressing_style: AddressingStyle) 
 async fn test_addressing_style_uri_fips(addressing_style: AddressingStyle) {
     run_test(|region| {
         let uri = format!("https://s3-fips.{region}.amazonaws.com");
-        Endpoint::from_uri(&uri, addressing_style).unwrap()
+        let endpoint = Endpoint::from_uri(&uri).unwrap();
+        EndpointConfig::new()
+            .region(region)
+            .addressing_style(addressing_style)
+            .endpoint(endpoint)
     })
     .await;
 }
@@ -88,7 +96,11 @@ async fn test_addressing_style_uri_fips(addressing_style: AddressingStyle) {
 async fn test_addressing_style_uri_fips_dualstack(addressing_style: AddressingStyle) {
     run_test(|region| {
         let uri = format!("https://s3-fips.dualstack.{region}.amazonaws.com");
-        Endpoint::from_uri(&uri, addressing_style).unwrap()
+        let endpoint = Endpoint::from_uri(&uri).unwrap();
+        EndpointConfig::new()
+            .region(region)
+            .addressing_style(addressing_style)
+            .endpoint(endpoint)
     })
     .await;
 }
